c4f531e165f0f43229f0673fea2e64402d53a843
TambyGit <hei.tamby@gmail.com>
2025-09-04 12:04:05 +0300

    feat: Second Test

--- src/test/java/TestFee.java
+++ src/test/java/TestFee.java
@@ -16,4 +16,11 @@ class TestFee {
         assertEquals(Status.PAID, fee.getStatus(Instant.now()));
     }
 
+    @Test
+    void getStatus_Late() {
+        Fee fee = new Fee(1, "Vacances HEI a Dubai", 230000, Instant.now().minusSeconds(100), new Student(1, "A", "B", null));
+        fee.addPayment(new Payment(1, 230000, Instant.now(), PaymentType.CASH));
+        assertEquals(Status.LATE, fee.getStatus(Instant.now()));
+    }
+
 }
